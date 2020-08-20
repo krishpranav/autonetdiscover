5c98271f5eeb90a914223005675a73291fffcc9c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-08-20 09:10:44 +0530

    Update autonetdiscover.py

--- autonetdiscover.py
+++ autonetdiscover.py
@@ -6,3 +6,9 @@ N = set(['no', 'n', 'NO', 'N'])
 
 interface = input("Enter Your Interface To Scan Your Network >> ")
 option = input("Are You Sure Do You Want To Scan Your Netowrk Y / N ")
+ 
+def scanner():
+  if option in Y:
+    print("-" * 50)
+    time.sleep(5)
+    print("Starting Your Netwo")
