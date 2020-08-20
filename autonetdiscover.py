b2d90925abc112c8609f51c18b6693303378c09a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2020-08-20 09:52:12 +0530

    Update autonetdiscover.py

--- autonetdiscover.py
+++ autonetdiscover.py
@@ -5,10 +5,21 @@ Y = set(['yes', 'y', 'YES', 'Y'])
 N = set(['no', 'n', 'NO', 'N'])
 
 interface = input("Enter Your Interface To Scan Your Network >> ")
-option = input("Are You Sure Do You Want To Scan Your Netowrk Y / N ")
- 
+option = input("Are You Sure Do You Want To Scan Your Network Y / N ")
+
 def scanner():
-  if option in Y:
-    print("-" * 50)
-    time.sleep(5)
-    print("Starting Your Netwo")
+    if option in Y:
+        print("-" * 50)
+        time.sleep(5)
+        print("Scanning Your Network")
+        time.sleep(5)
+        print("-" * 50)
+        time.sleep(5)
+        subprocess.call("sudo netdiscover -i" + interface, shell=True)
+    elif option in N:
+        print("Ok Bye...")
+        print("You Entered N...")
+        print("Exiting")
+        exit()
+
+scanner()
