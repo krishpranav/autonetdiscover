19410fbe9d5ca5d556c743093043b0e46e92de69
krishpranav <krisna.pranav@gmail.com>
2020-10-13 11:14:22 +0530

    done changes

--- autonetdiscover.py
+++ autonetdiscover.py
@@ -1,9 +1,7 @@
 #!usr/bin/env/python
-#simple python tool for scanning your network
-#make sure you have netdiscover 
-#author: krisna pranav
+#tool is created by krishpranav
 
-
+#imports
 import os
 import time
 
@@ -15,15 +13,13 @@ N = set(['no', 'n', 'NO', 'N'])
 interface = input("Enter Your Interface To Scan Your Network >> ")
 alert = input("Are You Sure You Want To Scan Your Network Y / N ")
 
-
 def scanner():
   if alert in Y:
-    os.system("clear")
-    time.sleep(2)
     os.system("figlet SCANNING | lolcat")
     time.sleep(2)
     os.system("sudo netdiscover -i" + interface)
   elif alert in N:
     os.system("figlet OK BYE | lolcat")
+    time.sleep(2)
     exit()
 scanner()
