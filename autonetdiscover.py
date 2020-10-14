1f5af0ed7735e27a4d18f31dff877fefe828c054
krishpranav <krisna.pranav@gmail.com>
2020-10-14 11:39:09 +0530

    done changes

--- autonetdiscover.py
+++ autonetdiscover.py
@@ -1,25 +1,28 @@
 #!usr/bin/env/python
-#tool is created by krishpranav
+'''
+tool name: autonetdiscover
+tool author: krishpranav
+'''
+
 
 #imports
 import os
 import time
 
-#define a set
 Y = set(['yes', 'y', 'YES', 'Y'])
 N = set(['no', 'n', 'NO', 'N'])
 
-#user input
 interface = input("Enter Your Interface To Scan Your Network >> ")
 alert = input("Are You Sure You Want To Scan Your Network Y / N ")
 
+
 def scanner():
-  if alert in Y:
-    os.system("figlet SCANNING | lolcat")
-    time.sleep(2)
-    os.system("sudo netdiscover -i" + interface)
-  elif alert in N:
-    os.system("figlet OK BYE | lolcat")
-    time.sleep(2)
-    exit()
+	if alert in Y:
+		os.system("figlet SCANNING | lolcat")
+		time.sleep(3)
+		os.system("sudo netdiscover -i" + interface)
+	elif alert in N:
+		os.system("figlet OK BYE")
+		time.sleep(1)
+		exit()
 scanner()
